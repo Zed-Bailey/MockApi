c71920cf9d7716166a29257fd5751d0f485e61f2
Zed <emailzed03@gmail.com>
2022-04-04 11:40:48 +1000

    removed dynamic object functions
    DynamicRow is now a class around a dictionary,

--- MockApi/Data/DynamicRow.cs
+++ MockApi/Data/DynamicRow.cs
@@ -6,22 +6,22 @@ namespace MockApi.Data;
 // https://docs.microsoft.com/en-us/dotnet/api/system.dynamic.dynamicobject?view=net-6.0
 //TODO: clean this class up to just use the dictionary as i am not using most of the features that the dynamic object provides
 
-public class DynamicRow : DynamicObject
+public class DynamicRow
 {
-    private Dictionary<string, object?> _dictionary = new();
+    public Dictionary<string, string?> _dictionary = new();
     
     // This property returns the number of elements
     // in the inner dictionary.
     public int Count => _dictionary.Count;
 
-    public object ValueFor(string key)
+    /// <summary>
+    /// Return the value for the passed in column key
+    /// </summary>
+    /// <param name="column">Name of column</param>
+    /// <returns>The value in the column, null if no value is in column</returns>
+    public string? ValueFor(string column)
     {
-        if (_dictionary.ContainsKey(key))
-        {
-            return _dictionary[key] ?? "";
-        }
-
-        return "";
+        return  _dictionary[column];
     }
 
     public string ToJson()
@@ -50,48 +50,9 @@ public class DynamicRow : DynamicObject
         }
     }
 
-    public bool Update(string key, object value)
+    public bool Update(string key, string value)
     {
         _dictionary[key] = value;
         return true;
     }
-    // If you try to get a value of a property
-    // not defined in the class, this method is called.
-    public override bool TryGetMember(
-        GetMemberBinder binder, out object? result)
-    {
-        // Converting the property name to lowercase
-        // so that property names become case-insensitive.
-        string name = binder.Name.ToLower();
-
-        // If the property name is found in a dictionary,
-        // set the result parameter to the property value and return true.
-        // Otherwise, return false.
-        return _dictionary.TryGetValue(name, out result);
-    }
-
-    // If you try to set a value of a property that is
-    // not defined in the class, this method is called.
-    public override bool TrySetMember(
-        SetMemberBinder binder, object? value)
-    {
-        // Converting the property name to lowercase
-        // so that property names become case-insensitive.
-        _dictionary[binder.Name.ToLower()] = value;
-
-        // You can always add a value to a dictionary,
-        // so this method always returns true.
-        return true;
-    }
-    
-    
-    /// <summary>
-    /// Overriding Equals is essential for use with Select and Table because they use HashSets internally
-    /// </summary>
-    public override bool Equals(object obj) => object.Equals(GetHashCode(), obj?.GetHashCode());
-
-    /// <summary>
-    /// Overriding GetHashCode is essential for use with Select and Table because they use HashSets internally
-    /// </summary>
-    public override int GetHashCode() => _dictionary.GetHashCode();
 }
