908b45b6c057ba1c4ae8c2c212047a630518499e
Zed <emailzed03@gmail.com>
2022-04-04 11:39:56 +1000

    add get methods for all rows and started work on the query method

--- MockApi/Controllers/DataController.cs
+++ MockApi/Controllers/DataController.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using MockApi.Data;
 using MockApi.Services;
-
+using System.Linq;
 namespace MockApi.Controllers;
 
 [ApiController]
@@ -10,6 +10,7 @@ public class DataController : ControllerBase
 {
     
     /*
+     * TODO: figure out how to have multiple get requests
      * Example queries
      * api/data?select=all&where=column&is=test
      * select * rows from data where column == test
@@ -23,20 +24,38 @@ public class DataController : ControllerBase
     public DataController(ILogger<DataController> logger, DataService service)
     {
         _logger = logger;
+        _service = service;
     }
 
-    [HttpGet]
-    public IActionResult Get([FromQuery(Name = "select")] string selectAmount, [FromQuery(Name = "where")] string columnName, [FromQuery(Name = "is")] string equalTo)
+    /// <summary>
+    /// Serialize the rows to json
+    /// </summary>
+    /// <param name="rows">rows to serialize</param>
+    /// <returns>a json string with the rows serialized in it</returns>
+    private string RowsToJson(IEnumerable<DynamicRow> rows)
     {
-        var invalidNumber = BadRequest(new {error = "invalid select amount passed in,  valid options are 'all' or int value > 0"});
+        // defintley a better way to do this, but this is working for now
+        var json = "[\n";
+        foreach (var r in rows) json += r.ToJson() + ",\n";
+        
+        json += "]";
+        return json;
+
+    }
+        
+
+    [HttpGet("select")]
+    public IActionResult GetQuery([FromQuery(Name = "limit")] string limit, [FromQuery(Name = "where")] string columnName, [FromQuery(Name = "is")] object equalTo)
+    {
+        var invalidNumber = BadRequest(new {error = "invalid limit amount passed in. valid options are 'all' to return all rows or an int value > 0"});
         
         var amount = -1;
-        if (selectAmount == "all")
+        if (limit == "all")
             amount = _service.Rows.Count;
         else
         {
             // try and parse the string into an int, if it fails return a bad request
-            if (!int.TryParse(selectAmount, out amount))
+            if (!int.TryParse(limit, out amount))
             {
                 return invalidNumber;
             }
@@ -48,6 +67,20 @@ public class DataController : ControllerBase
             
         }
         
-        return Ok();
+        // amount is now a valid value, check to see if the column exists
+        if (!_service.ColumnExists(columnName))  return BadRequest(new {error = $"The column with name {columnName} does not exist!"});
+    
+        var matched = _service.Rows.FindAll(x => x.ValueFor(columnName) == equalTo);
+        
+        return Ok(RowsToJson(matched));
+    }
+
+    [HttpGet]
+    public IActionResult GetAllRows()
+    {
+        if (_service.Rows.Count == 0) return Ok("[]");
+        
+        // return all the rows
+        return Ok(RowsToJson(_service.Rows));
     }
 }
